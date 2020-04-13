cb8616bb607ef974c59afa7bce3fb9e8151e0d0d
pavikpeter <47843452+pavikpeter@users.noreply.github.com>
2020-04-13 21:37:16 +0200

    new features added

--- scripts.js
+++ scripts.js
@@ -1,13 +1,24 @@
 let entryFieldText;
+let itemCount=0;
 
 $("#addButton").click(function(){
-    entryFieldText=$("#entryField").val();    
-    $("table").append("<tr><td id='item'>" + entryFieldText + "</td><td id='checked'></td> <td id='edit'></td><td id='delete'><button id='del'></button></td></tr>");
-    $("#entryField").val("");
+    entryFieldText=$("#entryField").val();   
+    if (entryFieldText!=='') {
+        if (itemCount<15) {
+            $("table").append("<tr><td id='item'>" + entryFieldText + "</td><td id='checked'></td> <td id='edit'></td><td id='delete'><button id='del'></button></td></tr>");
+            $("#entryField").val("");
+            itemCount++;
+        }else{
+            alert("List is full! No more items can be added!");
+        }       
+    }else{
+        alert("Enrty field is empty! Add some text!");
+    }
 });
 
 $("table").on("click", "#del", function() {
     $(this).closest("tr").remove();
+    itemCount--;
  });
 
 
